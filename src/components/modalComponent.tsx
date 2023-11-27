9b33ae80f9960c6b1fe8c0e0307e28fc3db0728a
Juan Carlos Avila <juanvs23@gmail.com>
2023-11-27 13:53:20 -0400

    version 1.1

--- src/components/modalComponent.tsx
+++ src/components/modalComponent.tsx
@@ -9,11 +9,23 @@ function ModalComponent({
   active: (title: string) => void;
 }) {
   const { title, prices, bed_rooms, completation_date, logo_image_url } = modal;
+  const dispatch = redux.hooks.useAppDispatch();
   const {
     message: { options },
   } = redux.hooks.useAppSelector((state) => state.form);
   return (
     <Styled.ModalItemWrapper>
+      <div className="close-modal">
+        <button onClick={() => dispatch(redux.ui.removeOption(modal.order))}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 384 512"
+          >
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+          </svg>
+        </button>
+      </div>
       <div className="left-section neghborhood-data">
         <div className="constr-price">
           <div className="logo-modal-container">
